b4a376a466c62456e4f4d8048fca33d5fc480493
Leopoldo Manfio <leopoldo.manfio@supero.com.br>
2019-12-19 19:24:29 -0200

    refactor to main js file

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const GPIO = require('onoff').Gpio;
-const awsIot = require('aws-iot-device-sdk')
+const awsIot = require('aws-iot-device-sdk');
 
-const testBerryOptions = {
-	keyPath: './certs/3be78d97bb-private.pem.key',
-	certPath: './certs/3be78d97bb-certificate.pem.crt',
-	caPath: './certs/root-CA.pem',
-	clientId: 'rasp_policy_test',
-	host: 'a276gfbq0dq4uz-ats.iot.us-east-1.amazonaws.com'
-}
+const testBerryOptions = require('./config/deviceOptions');
+
+// aux variables
 let clientTokenUpdate;
 let prevWet = false;
 
+// setting sdk
 const device = awsIot.device(testBerryOptions);
 const thingShadow = awsIot.thingShadow(testBerryOptions);
 
@@ -24,7 +21,7 @@ thingShadow.on('delta', (thingName, stateObject) => {
 
 thingShadow.on('timeout', (thingName, clientToken) => {
 	console.log(`received timeout on ${thingName} with token: ${clientToken}`);
-})
+});
 
 
 
@@ -40,46 +37,46 @@ device
 			
 			if (!clientTokenUpdate) console.log('updating shadow failed...');
 		});
-		})
+	});
 
 device.on('message', (topic, payload) => {
 	console.log('message', topic, payload.toString());
 });
 
+// settings GPIOS
 const wetSensor = new GPIO(18, 'out');
-
 const buzzer = new GPIO(17, 'out');
 buzzer.writeSync(0);
 
-// read buzzer state, watersensor and timestamp
+// read buzzer state, water sensor and timestamp
 const deviceState = {  };
-console.log('init device state');
 function initDeviceState() {
 	deviceState.buzzer = buzzer.readSync() ? 'HIGH' : 'OFF';
 	deviceState.waterSensor = wetSensor.readSync() ? 'HIGH' : 'OFF';
 	deviceState.timestamp = new Date().getTime();
-} initDeviceState();
+}
 
 function updateDeviceState() {
 	deviceState.buzzer = buzzer.readSync() ? 'HIGH' : 'OFF';
 	deviceState.waterSensor = wetSensor.readSync() ? 'HIGH' : 'OFF';
 	deviceState.timestamp = new Date().getTime();
 	
-	const testBerryState = {"reported":{"desired":{"waterSensor": deviceState.waterSensor, "buzzer": 'DISCONNECTED', "timestamp": deviceState.timestamp}}};		
+	const testBerryState = {"reported":{"desired":{"waterSensor": deviceState.waterSensor, "buzzer": 'DISCONNECTED', "timestamp": deviceState.timestamp}}};
+
+	// TODO: verify why not updating
 	clientTokenUpdate = thingShadow.update('TestBerry', testBerryState);
-	console.log('UPDATED TOKEN: ', clientTokenUpdate)
+	console.log('UPDATED TOKEN: ', clientTokenUpdate);
 }
 
 function sensor(condition) {
 	console.log('Attempting to handle sensor');
-		if (condition == 'wet') {
-			 // do something
+		if (condition === 'wet') {
 			device.publish('WaterSensorHigh', JSON.stringify({message: 'water level rose...', waterSensor: 'HIGH'}));
 			console.log('sensor got wet');
 			prevWet = true;
 			buzzer.writeSync(1);
 		}
-		if (condition == 'dry') {
+		if (condition === 'dry') {
 			// do something
 			console.log('sensor got dry or not sufficiently wet.');
 			prevWet = false;
@@ -101,19 +98,6 @@ function RCtime () {
 		}
 }
 
-function checkSensor() {
-	const wetReading = wetSensor.readSync();
-	if (wetReading) {
-		console.log('sensor is still wet');
-		clearInterval(checkSensor)
-	} else {
-		console.log("Sensor is dry again");
-		sensor('dry');
-		console.log('waiting for wetness...');
-		clearInterval(checkSensor)
-	}
-}
-
 function main() {
 	initDeviceState();
 	if (RCtime() === 1 && !prevWet) {
@@ -123,7 +107,8 @@ function main() {
 				console.log('sensor is still wet');
 			else {
 				sensor('dry');
-				clearInterval(refresh);
+				// TODO: verify need of clearInterval
+				// clearInterval(refresh);
 				console.log('waiting for wetness...');
 			}
 		}, 1000);
@@ -131,5 +116,6 @@ function main() {
 }
 
 // Main Loop
+initDeviceState();
 console.log('Waiting for wetness');
-setInterval(main, 10000)
+setInterval(main, 10000);
