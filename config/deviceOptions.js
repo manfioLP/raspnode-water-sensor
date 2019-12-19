decb9bf6b89e5ddb99f36108ef39e5f6e3187251
Leopoldo Manfio <leopoldo.manfio@supero.com.br>
2019-12-19 19:24:11 -0200

    create export options file

---
+++ config/deviceOptions.js
@@ -0,0 +1,9 @@
+const options = {
+	keyPath: './certs/3be78d97bb-private.pem.key',
+	certPath: './certs/3be78d97bb-certificate.pem.crt',
+	caPath: './certs/root-CA.pem',
+	clientId: 'rasp_policy_test',
+	host: 'a276gfbq0dq4uz-ats.iot.us-east-1.amazonaws.com'
+};
+
+module.exports = options;
